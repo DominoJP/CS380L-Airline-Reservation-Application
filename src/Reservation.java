48e881e2f53bf7cc4f1244cdb6060a69a8e1bd71
loganlang393 <145071293+loganlang393@users.noreply.github.com>
2023-10-05 12:23:54 -0700

    Update Reservation.java
    Finished creating the getReservation() method and also created a totalPrice variable along with a method that sets the value of it

--- src/Reservation.java
+++ src/Reservation.java
@@ -2,22 +2,26 @@
 import java.util.Scanner;
 
 public class Reservation {
-	private Account customer;
+	private Account customer; //whoever has the account and is making the reservation
 	private Flight flight;
-	private String[] passengers;
-	private int[] seatNumbers;
+	private String[] passengers; //total list of passengers in case if the reservation includes more than just the customer
+	private int[] seatNumbers; //an array containing the list of chosen seat numbers for the flight
+	private double totalPrice; //a double that keeps track of the total cost of this reservation since multiple tickets may be ordered
 	
+	//a constructor that accepts nothing
 	public Reservation() {
 		customer.name = null;
 		flight = null;
 	}
 	
+	//a constructor that adds the customer making the reservation and the flight the reservation applies to
 	 public Reservation(Account n, Flight f) {
 		 this.customer = n;
 		 this.flight = f;
 		 
 	 }
 	 
+	 //the method that sets all other values for the reservation
 	 public void setReservation() {
 		 Scanner scan = new Scanner(System.in);
 		 int numPassengers;
@@ -36,8 +40,9 @@ public class Reservation {
 			 System.out.println("");
 		 }
 		 
+		 //a for loop that is meant to allow each passenger to select what seat they want from what seats are still available on the flight
 		 for (int i = 1; i <= numPassengers; i++) {
-			 if(i == 1) {
+			 if(i == 1) { //I thought it might be good to give a different prompt to the customer making the reservation from the other passengers
 				 System.out.print("What seat would you like: ");
 				 seat = Integer.parseInt(scan.nextLine());
 				 System.out.println("");
@@ -64,10 +69,36 @@ public class Reservation {
 			 }
 		 }
 		 
+		 //print out the total price where the total is the price per ticket for the flight multiplied by the number of passengers on the reservation
+		 System.out.println("The total for this reservation is: " + this.setTotalPrice(numPassengers));
+		 
 		 return;
 	 }
 	 
+	 public double setTotalPrice(int p) {
+		 for(int i = 0; i < p; i++) {
+			 this.totalPrice = this.totalPrice + this.flight.pricing();
+		 }
+		 
+		 return this.totalPrice;
+	 }
+	 
+	 //prints all the set values for the reservation so that the customer is notified of their reservation
 	 public void getReservation() {
+		 System.out.println("Type of flight: " + this.flight.gettype());
+		 System.out.println("Destination: " + this.flight.cityArrival());
+		 System.out.println("City of Departure: " + this.flight.getcityDeparture());
+		 System.out.println("Date and time of departure: " + this.flight.dateDeparture()
+				 + " " + this.flight.timeDeparture());
+		 
+		 System.out.println("Number of Passengers(including customer): " + this.passengers.length);
+		 
+		 System.out.println("List of Passengers: \n");
+		 for(int i = 0; i < this.passengers.length; i++) {
+			 System.out.println("\t" + this.passengers[i]);
+		 }
+		 
+		 System.out.println("Total cost of reservation: " + this.totalPrice);
 		 
 		 return;
 	 }
