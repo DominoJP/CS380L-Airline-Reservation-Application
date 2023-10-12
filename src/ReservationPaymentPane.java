c91aefcd02e3a2bafe4ad368a5906964d3e1b0bc
Jevy Miranda <jevymira@outlook.com>
2023-10-12 15:48:44 -0700

    Add JPanel classes for reservation payment, history, and cancellation

---
+++ src/ReservationPaymentPane.java
@@ -0,0 +1,11 @@
+import javax.swing.JPanel;
+
+public class ReservationPaymentPane extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public ReservationPaymentPane() {
+
+	}
+
+}
