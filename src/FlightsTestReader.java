4aaaa6fcaca071cfdf1c3a675972bcd5f34b1cec
Jevy Miranda <jevymira@outlook.com>
2023-10-28 10:31:40 -0700

    feat(filtering): class FlightsTestReader using BufferedReaderto instantiate and add flights to FlightSorting Object

---
+++ src/FlightsTestReader.java
@@ -0,0 +1,53 @@
+// import Database.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+  Class that uses BufferedReader to instantiate flights line-by-line from .txt file.
+  Instantiates a FlightSorting Object to which instantiated flights are added.
+  @author Jevy Miranda
+  @version 1.0
+*/
+
+public class FlightsTestReader {
+	private String flightsFilePath = "src/Database/FlightsTest.txt";
+	private FlightSorting sort;
+	private Flight instantiatedFlight;
+	
+	public FlightsTestReader() {
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(flightsFilePath))) {
+		    String line;
+		    
+		    while ((line = reader.readLine()) != null) {
+		        String[] parts = line.split(", ");
+		        // Instantiate flight with parameters corresponding to String[] indices generated from the current line
+		        instantiatedFlight = new Flight(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], Integer.parseInt(parts[7]), Double.parseDouble(parts[8]));
+		        //FlightSorting instantiation requires a flight as parameter
+		        if (sort == null) {
+		        	sort = new FlightSorting(instantiatedFlight);
+		        } else {
+	        	    sort.addFlight(instantiatedFlight);
+		        }
+		    }
+		    
+		    reader.close();
+		    
+		} catch (IOException e) {
+		    e.printStackTrace();
+		
+		}
+	}
+	
+	/**
+	  Method getFlightSorting() returns the FlightSorting Object, 
+	  allowing invocation of methods sortFlights() and getList().
+	 */
+	
+	public FlightSorting getFlightSorting() {
+		return sort;
+	}
+	
+}
