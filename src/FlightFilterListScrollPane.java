2d81fcb6e05921537059d2aadadf07753727c934
Jevy Miranda <jevymira@outlook.com>
2023-10-27 21:48:37 -0700

    feat(filtering ui): JToolBar for navigation and sorting

--- src/FlightFilterListScrollPane.java
+++ src/FlightFilterListScrollPane.java
@@ -7,6 +7,8 @@ import javax.swing.JList;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JToolBar;
+import javax.swing.JComboBox;
 
 /**
    JPanel in BorderLayout with JScrollPane, allowing use of JList to display and select flights.
@@ -27,17 +29,6 @@ public class FlightFilterListScrollPane extends JPanel {
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, BorderLayout.CENTER);
 		
-		
-		JButton btnHeader = new JButton("Reserve Selected Flight");
-		btnHeader.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// FIXME
-				// use listScrollPaneTest.getSelectedValue();
-				((CardLayout) contentPane.getLayout()).show(contentPane, "PASSENGER1_DETAILS");
-			}
-		});
-		scrollPane.setColumnHeaderView(btnHeader);
-		
 		// SHOW: FLIGHT IDENTIFIER, PLACE OF DEPARTURE/ARRIVAL, TIME OF DEPARTURE/ARRIVAL, DURATION, CLASS COSt
 		// (DISREGARD) ADD CASE FOR NO FLIGHTS FOUND: RETOOL "RESERVE SELECTED FLIGHT" BUTTON INTO "GO BACK BUTTON"
 			// NEW DIRECTION: display "No Matches" message on previous JPanel (FlightFilterPane) and prompt to "Please try different search options."
@@ -45,6 +36,21 @@ public class FlightFilterListScrollPane extends JPanel {
 		JList listFlights = new JList(flightListSorted);
 		scrollPane.setViewportView(listFlights);
 		listFlights.setSelectedIndex(0);
+		
+		JToolBar toolBar = new JToolBar();
+		scrollPane.setColumnHeaderView(toolBar);
+		
+		JButton btnReturn = new JButton("Return");
+		toolBar.add(btnReturn);
+		
+		JComboBox comboBox = new JComboBox();
+		toolBar.add(comboBox);
+		
+		JButton btnSort = new JButton("Sort");
+		toolBar.add(btnSort);
+		
+		JButton btnBook = new JButton("Book Selected Flight");
+		toolBar.add(btnBook);
 		// returns Obj
 				
 	}
