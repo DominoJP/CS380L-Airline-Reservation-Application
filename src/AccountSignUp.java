eaa20027be90c6ce897ae09965391a32e6159bea
Sayra Reyes <sayrareyes@Sayras-MacBook-Air.local>
2023-11-02 15:13:18 -0700

    Added BufferedWriter

--- src/AccountSignUp.java
+++ src/AccountSignUp.java
@@ -1,8 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
+ * Account Sign Up - a program to read and write email and password pairs to a txt file.
+ * 
+ * This program checks if the "email password" exists in a text file and ensures that 
+ * the same email is no added multiple times. 
  * 
  * @author: Sayra Reyes 
  * @version: 1.0
@@ -10,16 +16,41 @@ import java.io.IOException;
 public class AccountSignUp {
 
 
+	
 	public static void main(String[] args) throws IOException {
-			
-	try(BufferedReader reader1 = new BufferedReader(new FileReader("TestAccountSignUp.txt"))){
+	
+	//check if the "email password" pair already exists. 
+	boolean pairExists = false;
+
+	//Create a BufferedReader to read from a file text. 
+	 try(BufferedReader reader1 = new BufferedReader(new FileReader("TestAccountSignUp.txt"))){
 		String line;
 		
 		while((line = reader1.readLine()) != null) {
-			System.out.println(line);
+			if(line.equals("email password")) { //checks if line matches "email password"
+				pairExists = true;
+				break;
+			}	
 		}
+		
 	} catch(IOException e) {
 		e.printStackTrace();
 	   }
-	}
+	
+	//If the pair does not exist, then add it to the file. 
+	if(!pairExists) {
+		
+	//Create a BufferedWriter to write email and password.
+	 try(BufferedWriter writer = new BufferedWriter(new FileWriter("TestAccountSignUp.txt", true))){
+			String str = "email" + "password";
+			
+			//Write the pair to the file and move to the next line. 
+			writer.write("\n" + str);
+			writer.close();
+
+	} catch(IOException e) {
+		e.printStackTrace();
+	     }
+      }	
+   }
 }
