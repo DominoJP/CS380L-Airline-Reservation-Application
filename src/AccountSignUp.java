7ce37663da6762db776553a3e57dc53aff645c06
Sayra Reyes <sayrareyes@Sayras-Air.lan>
2023-11-03 22:04:54 -0700

    Buffered Reader/Writer Account SignUp

--- src/AccountSignUp.java
+++ src/AccountSignUp.java
@@ -3,23 +3,46 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * 
- * @author: Sayra Reyes 
+ *
+ * @author: Sayra Reyes
  * @version: 1.0
  */
 public class AccountSignUp {
 
-
 	public static void main(String[] args) throws IOException {
-			
-	try(BufferedReader reader1 = new BufferedReader(new FileReader("TestAccountSignUp.txt"))){
+
+
+	//check if the "email password" pair already exists.
+	boolean pairExists = false;
+
+	//Create a BufferedReader to read from a file text.
+	 try(BufferedReader reader1 = new BufferedReader(new FileReader("TestAccountSignUp.txt"))){
 		String line;
-		
+
 		while((line = reader1.readLine()) != null) {
-			System.out.println(line);
+			if(line.equals("0001, name, @email, password, CUSTOMER")) { //checks if line matches "email" + "password".
+				pairExists = true;
+				break;
+			}
 		}
 	} catch(IOException e) {
 		e.printStackTrace();
 	   }
-	}
+
+	//If the pair does not exist, then add it to the file.
+	if(!pairExists) {
+
+	//Create a BufferedWriter to write email and password.
+	 try(BufferedWriter writer = new BufferedWriter(new FileWriter("TestAccountSignUp.txt", true))){
+			String str = "\t0001" + "," + "\tname" + "," + "\t@email.com" + "," + "\tpassword" + "," + "\tCUSTOMER";
+
+			//Write the pair to the file and move to the next line.
+			writer.write(str);
+			writer.close();
+
+	} catch(IOException e) {
+		e.printStackTrace();
+	     }
+      }
+   }
 }
