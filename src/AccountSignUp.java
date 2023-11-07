e7436aba91d1158cdc552e1ae1b957b8e56c9073
Sayra Reyes <sayrareyes@Sayras-MacBook-Air.local>
2023-11-07 15:12:26 -0800

    JavaMail API

--- src/AccountSignUp.java
+++ src/AccountSignUp.java
@@ -6,51 +6,54 @@ import java.io.IOException;
 
 /**
  * Account Sign Up - a program to read and write email and password pairs to a txt file.
- * 
- * This program checks if the "email password" exists in a text file and ensures that 
- * the same email is no added multiple times. 
- * 
- * @author: Sayra Reyes 
+ *
+ * This program checks if the "email password" exists in a text file and ensures that
+ * the same email is no added multiple times.
+ *
+ * @author: Sayra Reyes
  * @version: 1.0
  */
 public class AccountSignUp {
 
-
+	String password;
+	String email; 
 	
+
 	public static void main(String[] args) throws IOException {
-	
-	//check if the "email password" pair already exists. 
-	boolean pairExists = false;
 
-	//Create a BufferedReader to read from a file text. 
+	//check if the "email password" pair already exists.
+	boolean pairExists = false;
+	
+
+	//Create a BufferedReader to read from a file text.
 	 try(BufferedReader reader1 = new BufferedReader(new FileReader("TestAccountSignUp.txt"))){
 		String line;
-		
+
 		while((line = reader1.readLine()) != null) {
 			if(line.equals("email password")) { //checks if line matches "email password"
 				pairExists = true;
 				break;
-			}	
+			}
 		}
-		
+
 	} catch(IOException e) {
 		e.printStackTrace();
 	   }
-	
-	//If the pair does not exist, then add it to the file. 
+
+	//If the pair does not exist, then add it to the file.
 	if(!pairExists) {
-		
+
 	//Create a BufferedWriter to write email and password.
 	 try(BufferedWriter writer = new BufferedWriter(new FileWriter("TestAccountSignUp.txt", true))){
-			String str = "email" + "password";
-			
-			//Write the pair to the file and move to the next line. 
-			writer.write("\n" + str);
+			String str = "\t0001" + "," + "\tname" + "," + "\t@email.com" + "," + "\tpassword" + "," + "\tCUSTOMER";
+
+			//Write the pair to the file
+			writer.write(str);
 			writer.close();
 
 	} catch(IOException e) {
 		e.printStackTrace();
 	     }
-      }	
+      }
    }
 }
