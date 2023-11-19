dc87a765afe6b7ecc284d197ced8a8870dce8c67
Jevy Miranda <jevymira@outlook.com>
2023-11-18 19:03:37 -0800

    docs(sign-up-ui): header, Deliverable

--- src/SignUpPane.java
+++ src/SignUpPane.java
@@ -29,6 +29,17 @@ import java.awt.Color;
    @version 1.0
 */
 
+/**
+ * a) Design Documentation: "SignUpUI"
+ * b) Date Created: October 9, 2023
+ * c) @author Jevy Miranda, Sayra Reyes
+ * d) Description: JPanel subclass for new account sign up. JTextFields for all inputs.
+ * e) Functions: Displays JLabel error message with corresponding issue for the following issues:
+ * 	  name field empty, email invalid, password too short, and re-typed password does not match.
+ * f) Data Structures: N/A
+ * g) Algorithms: N/A
+ */
+
 public class SignUpPane extends JPanel {
 	private JLabel lblError;
 	
