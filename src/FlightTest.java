0111c0cd91b486bb0e292d6e82a7cfdf054031b9
Sayra Reyes <sayrareyes@Sayras-Air.lan>
2023-11-19 18:27:29 -0800

    FlightUnitTest

---
+++ src/FlightTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains unit tests for the Flight class. 
+ *
+ */
+class FlightTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		
+	}
+
+
+	@Test
+	void testToString() {
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
