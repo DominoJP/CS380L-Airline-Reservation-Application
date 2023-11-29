161d43456f6d81828908aa3cc36fbffb3bd73e0c
Jevy Miranda <jevymira@outlook.com>
2023-11-28 16:10:14 -0800

    restore Reservations.txt

--- src/PaymentPane.java
+++ src/PaymentPane.java
@@ -80,13 +80,19 @@ public class PaymentPane extends JPanel implements PropertyChangeListener {
 	
 	private String[] months = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
 	
+	private String[] cardType = {
+			"Credit",
+			"Debit"
+		};
 	
+	/*
 	private String[] cardType = {
 		"Visa",
 		"Mastercard",
 		"American Express",
 		"Discover",
 	};
+	*/
 	
 	
 	/**
