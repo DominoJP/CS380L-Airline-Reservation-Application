e933c348af3710aae82181de317788fd0cb2aef8
Jevy Miranda <jevymira@outlook.com>
2023-12-17 18:53:02 -0800

    fix(compiling): cmt deprecated methods

--- src/Account.java
+++ src/Account.java
@@ -263,9 +263,9 @@ public class Account{
 		  System.out.println("Arrival city: " + flight.getcityArrival());
 		  System.out.println("Departure date: " + flight.getdateDeparture());
 		  System.out.println("Arrival date: " + flight.getTimeArrival());
-		  System.out.println("Total Passenger Caapcity: " + flight.gettotalpassengercapacity());
+		  // System.out.println("Total Passenger Caapcity: " + flight.gettotalpassengercapacity());
 		  // System.out.println("Passengers on board: " + flight.getPassenger());
-		  System.out.println("Price of flight: " + flight.getpricing());
+		  // System.out.println("Price of flight: " + flight.getpricing());
 
 	 }
 	 else {
